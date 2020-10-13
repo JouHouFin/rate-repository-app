d9af46ac8869ebf079bce0e8954204e3f10a4ec0
Juho Holmi <juhoholmi@gmail.com>
2020-10-13 13:18:49 +0300

    Started part10 and Exercise 10.1: initializing the application done

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Holy cow! I just started HY Full stack part 10!</Text>
       <StatusBar style="auto" />
     </View>
   );
