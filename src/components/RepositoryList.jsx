2ce6f36aeec936a16c3159ceb04b96917f4b0b48
Juho Holmi <juhoholmi@gmail.com>
2020-11-18 16:01:07 +0200

    Searchbar added to the list

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,10 +4,16 @@ import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import { useHistory } from 'react-router-dom';
 import RNPickerSelect from 'react-native-picker-select';
+import { Searchbar } from 'react-native-paper';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
+  },
+  searchBar: {
+    marginHorizontal: 10,
+    marginTop: 10,
+    height: 40
   }
 });
 
@@ -22,33 +28,68 @@ const Dropdown = ({ setOrder, order }) => {
         { label: 'Highest rated repositories', value: { mode: 'RATING_AVERAGE', order: 'DESC' } },
         { label: 'Lowest rated repositories', value: { mode: 'RATING_AVERAGE', order: 'ASC' } },
       ]}
-      value={order}
     />
   );
 };
 
-export const RepositoryListContainer = ({ repositories, loading, handlePress, setOrder, order }) => {
-  // Get the nodes from the edges array
-  const repositoryNodes = repositories
-    ? repositories.edges.map(edge => edge.node)
-    : [];
+const FilterBar = () => {
+  const [searchQuery, setSearchQuery] = React.useState('');
 
-
-  if (loading) return <Text>Loading</Text>;
+  const onChangeSearch = query => setSearchQuery(query);
 
   return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      ListHeaderComponent={Dropdown({ setOrder, order })}
-      renderItem={({ item }) => (
-        <TouchableOpacity onPress={() => handlePress(item)}>
-          <RepositoryItem item={item} viewSingle={false}/>
-        </TouchableOpacity>
-      )}
+    <Searchbar
+      placeholder="Search for a repository"
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+      style={styles.searchBar}
     />
   );
 };
+
+const RepositoryListHeader = ({ setOrder, order }) => {
+  return (
+    <View>
+      <FilterBar style={styles.searchBar} />
+      <Dropdown setOrder={setOrder} order={order} style={styles.searchBar}/>
+    </View>
+  );
+};
+
+export class RepositoryListContainer extends React.Component {
+  renderHeader = () => {
+    // this.props contains the component's props
+    const props = this.props;
+    const order = props.order;
+    const setOrder = props.setOrder;
+
+    return (
+      <RepositoryListHeader order={order} setOrder={setOrder} />
+    );
+  };
+
+  render() {
+    const props = this.props;
+    const repositories = props.repositories;
+    const repositoryNodes = repositories
+      ? repositories.edges.map(edge => edge.node)
+      : [];
+    const handlePress = props.handlePress;
+
+    return (
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        ListHeaderComponent={this.renderHeader}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => handlePress(item)}>
+            <RepositoryItem item={item} viewSingle={false}/>
+          </TouchableOpacity>
+        )}
+      />
+    );
+  }
+}
 
 const RepositoryList = ({ order, setOrder }) => {
   const { repositories, loading } = useRepositories(order);
@@ -57,8 +98,9 @@ const RepositoryList = ({ order, setOrder }) => {
   const handlePress = (item) => {
     history.push(`/repository/${item.id}`);
   };
+  if (loading) return <Text>Loading</Text>;
 
-  return <RepositoryListContainer repositories={repositories} loading={loading} handlePress={handlePress} setOrder={setOrder} order={order}/>;
+  return <RepositoryListContainer repositories={repositories} handlePress={handlePress} setOrder={setOrder} order={order}/>;
 
 };
 
